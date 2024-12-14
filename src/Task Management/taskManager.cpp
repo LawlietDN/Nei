290cdd008357d90ce62d73720ea9820fd758db48
LawlietDN <ufcgamer82@gmail.com>
2024-12-14 18:01:29 +0300

    Added some command handling, and assignments.

--- src/Task Management/taskManager.cpp
+++ src/Task Management/taskManager.cpp
@@ -6,21 +6,33 @@ namespace util
     util::TaskManager::TaskManager() = default;
     void util::TaskManager::manageTaskArguments(int argc, char* argv[])
     {
+        TaskData TaskData;
         cli::ArgumentParse parser;
         std::vector<std::string> arguments = parser.passArgument(argc, argv);
 
         if (arguments[0] == "add")
         {
             arguments = cmdHandle::CommandHandler::validateAddCommand(arguments);
+            TaskData.task = arguments[1];
+            int lastID = //parseLastID() //Implementing this soon
+            TaskData.taskID = cmdHandle::CommandHandler::taskIDGenerator(lastID);
+            if(arguments.size() > 2)
+            {
+                TaskData.description = arguments[3];
+            }
             }
             else if(arguments[0] == "delete")
             {
                 arguments = cmdHandle::CommandHandler::validateDeleteCommand(arguments);
-            }
-           
-        
-        
 
+            }
+            else if(arguments[0] == "markp")
+            {
+                arguments = cmdHandle::CommandHandler::validateMarkPCommand(arguments);
+            }
+
+            std::cout << "Task is: " << TaskData.task << '\n';
+            std::cout << "Description is: " << TaskData.description << '\n';
          for(auto argument: arguments)
          {
             std::cout << argument << '\n';
