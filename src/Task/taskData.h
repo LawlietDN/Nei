359e053b377056bb86880fe22d702d6ca42805b8
LawlietDN <ufcgamer82@gmail.com>
2024-12-14 18:00:16 +0300

    Changed from class to struct.

--- src/Task/taskData.h
+++ src/Task/taskData.h
@@ -1,11 +1,15 @@
 #pragma once
 #include <string>
+#include <vector>
+
 
 struct TaskData {
     int taskID;
+    std::string task;
     std::string description;
     std::string status;
     std::string createdAt;
     std::string updatedAt;
+    std::vector<std::string> taskList;
 };
 
