993b502a205bb1ff23a6a0b78c203bba38215018
LawlietDN <ufcgamer82@gmail.com>
2024-12-15 16:31:29 +0300

    Implemented partially incomplete function definition for parsing task data into a JSON file.

--- src/JSON/json.cpp
+++ src/JSON/json.cpp
@@ -6,12 +6,45 @@ namespace json
     json::parse::parse() = default;
 
 
-    void json::parse::parseToJson()
+    void json::parse::parseToJson(TaskData& TaskData)
     {
+        
         std::string fileName = "TaskList.json";
         std::ofstream file(fileName);
+        std::ifstream ifile(fileName);
+        std::string existingContent, line;
+
         initializeJSONfile(fileName);
+        while (std::getline(ifile, line)) {
+            existingContent += line;
+        }
+        ifile.close();
+
+        if (!existingContent.empty() && existingContent.back() == ']') {
+            existingContent.pop_back(); 
+            if (existingContent.size() > 1) {
+                existingContent += ",\n"; 
+            }
+        } else {
+            existingContent = "[\n\n";                                          //INCOMPLETE
+        }
+        if(file)
+        {
+            file << existingContent;
+            file << "    {\n"
+                    << "        \"ID\": " << TaskData.taskID << ",\n"
+                    << "        \"Task Name\": \"" << TaskData.task << "\",\n"
+                    << "        \"Description\": \"" << TaskData.description << "\",\n"
+                    << "        \"Created_at\": \"" << TaskData.createdAt << "\",\n"
+                    << "        \"Updated_at\": \"" << TaskData.updatedAt << "\",\n"
+                    << "        \"status\": \"" << TaskData.status << "\"\n"
+                    << "    }";
+
+            file << "\n\n]";
+            file.close();
+        }
         
+
     }
 
 
