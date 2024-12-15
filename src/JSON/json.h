9242694ecb8220dc37a1ff99f27a2250c685a812
LawlietDN <ufcgamer82@gmail.com>
2024-12-15 22:54:19 +0300

    Transformed the file name variabe previously in json.cpp to a private member && Added function declaration for trimJSOON function, and definitions in json.cpp.

--- src/JSON/json.h
+++ src/JSON/json.h
@@ -10,11 +10,14 @@ namespace json
 {
     class parse
     {
+    private:
+        inline static const std::string fileName = "TaskList.json";
 
     public:
         parse();
 
         static int initializeJSONfile(std::string const& fileName);
         static void parseToJson(TaskData& TaskData);
+        static std::string trimJSON(std::string const& existingContent);
     };
 }
