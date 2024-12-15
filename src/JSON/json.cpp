944f48376760f330c7744b300d9b4593dbbb904b
LawlietDN <ufcgamer82@gmail.com>
2024-12-15 14:51:51 +0300

    Beginning JSON operations. Check point.

--- src/JSON/json.cpp
+++ src/JSON/json.cpp
@@ -3,8 +3,24 @@
 
 namespace json
 {
-    json::parse::parse()
+    json::parse::parse() = default;
+
+
+    void json::parse::parseToJson()
     {
+        std::string fileName = "TaskList.json";
+        std::ofstream file(fileName);
+        initializeJSONfile(fileName);
         
+    }
+
+
+    int json::parse::initializeJSONfile(std::string const& fileName)
+    {
+        if(std::filesystem::exists(fileName)) {return 1;}
+
+        std::ofstream newFile(fileName);
+        newFile << "[]";
+        newFile.close();
     }
 };
--- src/JSON/json.h
+++ src/JSON/json.h
@@ -1,9 +1,17 @@
 #include "Task/taskData.h"
+#include <filesystem>
+#include <fstream>
+
+
+
 
 namespace json
 {
     class parse
     {
         parse();
+
+        int initializeJSONfile(std::string const& fileName);
+        void parseToJson();
     };
 }
