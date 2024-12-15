d8bd29c12588183b9a2d4c124d70c3ef82764cd9
LawlietDN <ufcgamer82@gmail.com>
2024-12-15 12:30:54 +0300

    Moved a error displaying message function to utility.h

--- src/CLI/Command-Validation/cmdValidator.h
+++ src/CLI/Command-Validation/cmdValidator.h
@@ -8,9 +8,7 @@ namespace cmdHandle
         CommandHandler();
 
         static std::vector<std::string> validateAddCommand(std::vector<std::string> const& args);
-        static std::vector<std::string> validateDeleteCommand(std::vector<std::string> const& args);
-        static void InvalidIDmessage(std::ostream& os);
-        static std::vector<std::string> validateMarkPCommand(std::vector<std::string> const& args);
+        static std::vector<std::string> validateMarkpAndDeleteAndMarkdCommand(std::vector<std::string> const& args);
         static int taskIDGenerator(int lastID);
     };
 }
