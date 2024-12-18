e51189c77b1f09f4412f39d9d845d19c1fe74d69
LawlietDN <ufcgamer82@gmail.com>
2024-12-19 00:44:26 +0300

    Function declaration for both updateTask and displayTaskList

--- src/JSON/json.h
+++ src/JSON/json.h
@@ -1,3 +1,4 @@
+#pragma once
 #include "Task/taskData.h"
 #include "Utility/utility.h"
 #include <filesystem>
@@ -19,11 +20,14 @@ namespace json
         static int initializeJSONfile();
         static void parseToJSON(TaskData& TaskData);
         static std::string trimJSON(std::string const& existingContent);
-        static void updateJSON(std::string const& argument, int ID);
+        static void updateJSON(std::vector<std::string> const& argument, int ID, TaskData& TaskData);
         static int getIncrementedLastID();
         static bool isTaskExist(std::string const& taskName);
         static bool isIDExist(int taskID);
         static void deleteTask(int taskID);
         static std::vector<std::string> loadFileToVector();
+        static void displayTaskList();
+        static void updateTask(std::string const& argument, int taskID,  TaskData& TaskData);
+
     };
 }
